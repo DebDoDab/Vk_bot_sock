8e6a3eb1509e39e88f811d153a657bf7c2da5fa5
VadOS <34406365+DebDoDab@users.noreply.github.com>
2020-04-29 00:12:48 +0300

    Deleted file with keys

--- settings.py
+++
@@ -1,9 +0,0 @@
-token = '3cc4ee0adfef428c7cad374151ccc0bfbbd4dc9c68fb353bc4eb8370a97f1e453dcf634c644e861921301'
-confirmation_token = '6aa984d9'
-
-user_tickets = {}
-user_leave = []
-last_msg = {}
-
-main_admin = 177933956
-id_group = 160224039
